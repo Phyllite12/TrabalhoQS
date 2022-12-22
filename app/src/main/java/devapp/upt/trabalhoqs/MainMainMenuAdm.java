ab7fc80d94318337e09127facf9decb840c7eeb3
RafaelPereira86 <carlosrafael@live.com.pt>
2022-12-22 12:28:36 +0000

    listar material e alguns layouts

--- app/src/main/java/devapp/upt/trabalhoqs/MainMainMenuAdm.java
+++ app/src/main/java/devapp/upt/trabalhoqs/MainMainMenuAdm.java
@@ -3,16 +3,21 @@ package devapp.upt.trabalhoqs;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMainMenuAdm extends AppCompatActivity {
 
     CardView c10,c11 ,c12 ,c13 ,c14 ,c15;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuadm);
+
+        i = getIntent();
 
         c10 = findViewById(R.id.c10);
         c11 = findViewById(R.id.c11);
@@ -20,5 +25,37 @@ public class MainMainMenuAdm extends AppCompatActivity {
         c13 = findViewById(R.id.c13);
         c14 = findViewById(R.id.c14);
         c15 = findViewById(R.id.c15);
+
+
+        c10.setOnClickListener(this :: );
+        c11.setOnClickListener(this :: );
+        c12.setOnClickListener(this :: );
+        c13.setOnClickListener(this :: );
+        c14.setOnClickListener(this :: );
+        c15.setOnClickListener(this :: voltar);
+    }
+
+    private void requisicoes(View view){
+        i = new Intent(this,ListarRequisicoes.class);
+        startActivity(i);
+        ;
+    }
+
+    private void (View view){
+        i = new Intent(this,Listar.class);
+        startActivity(i);
+    }
+
+    private void (View view){
+    }
+
+    private void (View view) {
+    }
+
+    private void (View view){
+    }
+
+    private void voltar(View view){
+        finish();
     }
 }
--- app/src/main/java/devapp/upt/trabalhoqs/MainMenu.java
+++ app/src/main/java/devapp/upt/trabalhoqs/MainMenu.java
@@ -3,11 +3,14 @@ package devapp.upt.trabalhoqs;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
     CardView c1 ,c2 ,c3 ,c4 ,c5 ,c6;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,39 @@ public class MainMenu extends AppCompatActivity {
         c4 = findViewById(R.id.c4);
         c5 = findViewById(R.id.c5);
         c6 = findViewById(R.id.c6);
+
+        c1.setOnClickListener(this :: );
+        c2.setOnClickListener(this :: );
+        c3.setOnClickListener(this :: );
+        c4.setOnClickListener(this :: );
+        c5.setOnClickListener(this :: );
+        c6.setOnClickListener(this :: voltar);
+    }
+
+    private void (View view){
+        i = new Intent(this,ListarRequisicoes.class);
+        startActivity(i);
+        ;
+    }
+
+    private void listarMaterais(View view){
+        i = new Intent(this,ListarMaterial.class);
+        startActivity(i);
+    }
+
+    private void (View view){
+    }
+
+    private void (View view) {
+    }
+
+    private void (View view){
+    }
+
+    private void voltar(View view){
+        finish();
+    }
+}
 
 
     }
